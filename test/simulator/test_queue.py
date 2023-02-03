739770af92e46db17f9ab2618c27e4d9ba370da1
Vilmen Abramian <abramian.vl@phystech.edu>
2023-02-03 16:33:39 +0300

    Improve tests (after code review)

--- test/simulator/test_queue.py
+++ test/simulator/test_queue.py
@@ -2,23 +2,34 @@ from pysim.sim import EventQueue
 import pytest
     
 
-def test_push():
+def test_push_pop_single():
     queue = EventQueue()
-    queue.push(1, 'event')
-    assert not queue.empty and queue.__len__() == 1
+    ev_id = queue.push(1, 'cat')
+
+    assert len(queue) == 1
+    assert (1, ev_id, 'cat') == queue.pop()
+    assert len(queue) == 0
 
 def test_pop():
     queue = EventQueue()
-    queue.push(2, 'event_0')
-    queue.push(4.5, 'event_1')
-    queue.push(3, 'event_2')
-    queue.push(1, 'event_3')
-    queue.push(3, 'event_4')
-    res = []
-    while queue.__len__() != 0:
-        res.append(queue.pop())
-    assert res[0][0]<=res[1][0]<=res[2][0]<=res[3][0]<=res[4][0] and res[2][1] < res[3][1]
+    queue.push(1, 'cat')        # 1
+    queue.push(3, 'dog')        # 2
+    queue.push(2, 'cow')        # 3
+    queue.push(4, 'horse')      # 4
+    queue.push(6, 'rooster')    # 5
 
+    res = []
+    res.append(queue.pop())
+    res.append(queue.pop())
+    res.append(queue.pop())
+    res.append(queue.pop())
+    res.append(queue.pop())
+
+    assert res[0][2] == 'cat'
+    assert res[1][2] == 'cow'
+    assert res[2][2] == 'dog'
+    assert res[3][2] == 'horse'
+    assert res[4][2] == 'rooster'
 
 # def test_void_queue_pop():
 #     # TODO: Добавить проверку на вылет ошибки в случае пустой очереди   
@@ -26,30 +37,53 @@ def test_pop():
 
 def test_cancel():
     queue = EventQueue()
-    id_list = []
-    for i in range(10):
-        id_list.append(queue.push(i, 'event')) 
-    queue.cancel(id_list[2])
-    assert queue.__len__() == 9
-    queue.cancel(id_list[2])
-    assert queue.__len__() == 9
+    queue.push(1, 'cat')        # 1
+    queue.push(3, 'dog')        # 2
+    queue.push(2, 'cow')        # 3
+    queue.push(4, 'horse')      # 4
+    queue.push(6, 'rooster')    # 5
+    queue.cancel(2)             # cow
+    queue.cancel(6)             # rooster
+
+    assert (1, 0, 'cat') == queue.pop()
+    assert (3, 1, 'dog') == queue.pop()
+    assert (4, 3, 'horse') == queue.pop()
+    assert (6, 4, 'rooster') == queue.pop()
 
 def test_len():
     queue = EventQueue()
-    for i in range(10):
-        queue.push(i, 'event')
-    assert queue.__len__() == 10
+    queue.push(1, 'cat')        # 1
+    queue.push(3, 'dog')        # 2
+    queue.push(2, 'cow')        # 3
+    queue.push(4, 'horse')      # 4
+    queue.push(6, 'rooster')    # 5
     queue.cancel(0)
     queue.cancel(1)
-    assert queue.__len__() == 8
-    queue.cancel(1)
-    assert queue.__len__() == 8
-    queue.push(15, 'new_event')
-    assert queue.__len__() == 9
+
+    assert queue.__len__() == 3
+
+    queue.cancel(3)
+    assert queue.__len__() == 2
 
 def test_clear():
     queue = EventQueue()
-    for i in range(10):
-        queue.push(i, 'event')
+    queue.push(1, 'cat')        # 1
+    queue.push(3, 'dog')        # 2
+    queue.push(2, 'cow')        # 3
+    queue.push(4, 'horse')      # 4
+    queue.push(6, 'rooster')    # 5
     queue.clear()
-    assert queue.__len__() != queue.empty
+
+    assert len(queue) != queue.empty
+
+def test_cancel_not_existing():
+    queue = EventQueue()
+    queue.push(1, 'cat')        # 1
+    queue.push(3, 'dog')        # 2
+    queue.push(2, 'cow')        # 3
+    queue.cancel(0)
+    queue.cancel(0)
+    queue.cancel(1000)
+
+    assert (2, 2, 'cow') == queue.pop()
+    assert (3, 1, 'dog') == queue.pop()
