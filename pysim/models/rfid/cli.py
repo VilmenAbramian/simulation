0cf13266a55304b2aec73e924d89a91dddeb4d21
Vilmen <abramian.vl@phystech.edu>
2024-06-27 17:41:52 +0300

    Add auto plotting graphs

--- pysim/models/rfid/cli.py
+++ pysim/models/rfid/cli.py
@@ -7,20 +7,20 @@ import epcstd as std
 from processing import result_processing
 
 
-DEFAULT_SPEED = 10             # kmph
+DEFAULT_SPEED = 30             # kmph
 DEFAULT_ENCODING = '2'         # FM0, M2, M4, M8
-DEFAULT_TARI = "12.5"          # 6.25, 12.5, 18.75, 25
+DEFAULT_TARI = '12.5'          # 6.25, 12.5, 18.75, 25
 DEFAULT_USE_TREXT = False      # don't use extended preamble
 DEFAULT_USE_DOPPLER = True     # simulate Doppler shift
 DEFAULT_FREQUENCY = 860.0      # reader frequency in MHz
 DEFAULT_Q = 2                  # Q parameter
 DEFAULT_NUM_TAGS = 1_000       # number of tags to simulate
 DEFAULT_TID_WORD_SIZE = 64     # number of words to read from TID (=1024 bits)
-DEFAULT_READER_OFFSET = 5.0    # meters from the wall
-DEFAULT_TAG_OFFSET = 5.0       # meters from the wall
+DEFAULT_READER_OFFSET = 3.0    # meters from the wall
+DEFAULT_TAG_OFFSET = 2.0       # meters from the wall
 DEFAULT_ALTITUDE = 5.0
 DEFAULT_NUM_TAGS = 100
-DEFAULT_POWER = 31.5
+DEFAULT_POWER = 29
 
 
 # ----------------------------------------------------------------------------
@@ -71,9 +71,6 @@ def cli():
 @click.option(
     '-n', '--num-tags', default=DEFAULT_NUM_TAGS, show_default=True,
     help='Number of tags to simulate.')
-@click.option(
-    '-j', '--jobs', default=multiprocessing.cpu_count(), show_default=True,
-    help='Number of parallel jobs to run when multiple arguments are given.')
 @click.option(
     '-v', '--verbose', is_flag=True, default=False, show_default=True,
     help='Print additional data, e.g. detailed model configuration.')
--- pysim/models/rfid/processing.py
+++ pysim/models/rfid/processing.py
@@ -1,4 +1,5 @@
 import json
+import matplotlib.pyplot as plt
 from tabulate import tabulate
 import time
 
@@ -8,7 +9,7 @@ def result_processing(initial_data, results, variadic, save_results=False):
     Обработка полученных данных из модели.
     Если была запущена только одна симуляция, то
     результаты выводятся в консоль. В противном случае
-    данные можно сохранить в файл или построить по ним график.
+    данные можно сохранить в файл и построить по ним график.
 
     Если save_results = True, то входные и
     выходные данные сохраняются в .txt файл в
@@ -21,43 +22,88 @@ def result_processing(initial_data, results, variadic, save_results=False):
         print(f'Время выполнения симуляции: {results[1]} с.')
     elif isinstance(results, list):
         # Результаты работы запуска нескольких симуляций
-        print('initial_data: ', initial_data)
-        print('results: ', results)
-        print('variadic: ', variadic)
+        initial_data[variadic] = sorted(set(initial_data[variadic]))
+        print_the_mult_results_to_the_terminal(initial_data, results, variadic)
+        if save_results:
+            save_mult_results_to_file(initial_data, results, variadic)
+        plot_results(initial_data, results, variadic)
 
 
+def separate_res(results):
+    ret = []
+    time_list = []
+    for i in range(len(results)):
+        ret.append(results[i][0])
+        time_list.append(results[i][1])
+    return ret, time_list
 
 
-    # result['encoding'] = encoding.name
-    # result['tari'] = f"{tari * 1e6:.2f}"
-    # result['speed'] = speed
-    # result['tid_word_size'] = tid_word_size
-    # result['reader_offset'] = reader_offset
-    # result['tag_offset'] = tag_offset
-    # result['altitude'] = altitude
-    # result['power'] = power
-      
-      
-#         # Результаты выводим в двух таблицах: таблице параметров и
-#         # таблице результатов. В последней - значение изменяющегося аргумента
-#         # и результаты, которые ему соответсвуют.
-#         params_names = list(var_arg_names) + ["encoding", "tari", "num_tags"]
-#         params_names.remove(variadic)
+def results_to_dict(ret):
+    results_dict = {'read_tid_prob': [], 'inventory_prob': [], 'rounds_per_tag':[]}
+    for i in range(len(ret)):
+        results_dict['read_tid_prob'].append(ret[i]['read_tid_prob'])
+        results_dict['inventory_prob'].append(ret[i]['inventory_prob'])
+        results_dict['rounds_per_tag'].append(ret[i]['rounds_per_tag'])
+    return results_dict
 
-#         print("\n# PARAMETERS:\n")
-#         print(tabulate([(name, kwargs[name]) for name in params_names],
-#                        tablefmt='pretty'))
+def print_the_mult_results_to_the_terminal(initial_data, results, variadic):
+    '''
+    Результаты выводим в двух таблицах: таблице параметров и
+    таблице результатов. В последней - значение изменяющегося аргумента
+    и результаты, которые ему соответсвуют.
+    '''
+    params_names = [
+        'speed', 'tid_word_size', 'altitude', 'reader_offset',
+        'tag_offset', 'power', 'encoding', 'tari', 'num_tags']
+    params_names.remove(variadic)
 
-#         # Подготовим таблицу результатов.
-#         # Какие ключи нужны из словарей в списке ret (который вернул pool.map):
-#         # FIXME: ошибка в добавлении столбца с изменяющимся параметром
-#         # ret_cols = (variadic, "read_tid_prob", "inventory_prob",
-#         #             "rounds_per_tag")
-#         ret_cols = ("read_tid_prob", "inventory_prob",
-#                     "rounds_per_tag")
-#         print('ret_cols :', ret_cols)
-#         print('ret: ', ret)
-#         # Строки таблицы результатов:
-#         results_table = [[item[column] for column in ret_cols] for item in ret]
-#         print("\n# RESULTS:\n")
-#         print(tabulate(results_table, headers=ret_cols, tablefmt='pretty'))
+    ret, time_list = separate_res(results)
+
+    print("\n# STATIC PARAMETERS:\n")
+    print(tabulate([(name, initial_data[name]) for name in params_names],
+                   tablefmt='pretty'))
+
+    # Подготовим таблицу результатов.
+    # Какие ключи нужны из словарей в списке ret (который вернул pool.map):
+    ret_cols = ['read_tid_prob', 'inventory_prob', 'rounds_per_tag']
+    # Строки таблицы результатов:
+    results_table = [[item[column] for column in ret_cols] for item in ret]
+    ret_cols.insert(0, variadic)
+    for i in range(len(initial_data[variadic])):
+        results_table[i].insert(0, initial_data[variadic][i])
+
+    print("\n# RESULTS:\n")
+    print(tabulate(results_table, headers=ret_cols, tablefmt='pretty'))
+    print(f'Среднее время одной симуляции = {sum(time_list)/len(time_list)} с')
+
+
+def save_mult_results_to_file(initial_data, results, variadic):
+    current_time = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    filename = 'results/text/' + 'sim_res-' + current_time + '.txt'
+    ret, time_list = separate_res(results)
+    results_dict = results_to_dict(ret)
+    with open(filename, 'w') as f:
+        f.write('Входные параметры: ' + json.dumps(initial_data) + '\n')
+        f.write('Результаты моделирования: ' + json.dumps(results_dict))
+
+def plot_results(initial_data, results, variadic, save_fig = True):
+    ret, _ = separate_res(results)
+    results_dict = results_to_dict(ret)
+    current_time = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    filename = 'results/plots/' + current_time + '-' + variadic + '.png'
+    units = {'speed': 'km/h', 'reader_offset': 'm', 'altitude': 'm', 'power': 'dBm', 'tid_word_size': 'words'}
+    
+    fig, ax = plt.subplots(figsize=(14, 8), layout='constrained')
+    ax.plot(
+        initial_data[variadic], results_dict['read_tid_prob'],
+        linewidth=3, linestyle='-',
+        marker='s', markevery=30,
+        markersize=8, label=initial_data['encoding'] + ', Tari = ' + initial_data['tari'] + ' µs'
+    )
+    ax.legend(fontsize=28)
+    ax.set_ylabel('Probability of reading', fontsize=31)
+    ax.set_xlabel(variadic + ', ' + units[variadic], fontsize=31)
+    ax.tick_params(axis='both', which='major', labelsize=31)
+    ax.grid()
+    if save_fig:
+        fig.savefig(filename)
