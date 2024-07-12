f25c2a7d12bba2685ba36766512ff56987333b5d
Vilmen <abramian.vl@phystech.edu>
2024-07-12 14:25:05 +0300

    Update setup

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name="pyrfidsim",
-    version="0.1.0",
-    author="Andrey Larionov",
-    author_email="larioandr@gmail.com",
-    platforms=["any"],
-    license="MIT",
-    url="https://github.com/ipu69/pyrfidsim",
-    packages=["pysim"],
+    name='pyrfidsim',
+    version='1.0.0',
+    author='Vilmen Abramian',
+    author_email='abramian.vl@phystech.edu',
+    platforms=['any'],
+    license='MIT',
+    url='https://github.com/VilmenAbramian/simulation',
+    packages=['pysim'],
     install_requires=[
-        "click",
-        "colorama",
-        "pydantic",
+        'click',
+        'colorama',
+        'pydantic',
     ],
     test_requires=[
-        "pytest",
+        'pytest',
     ],
     entry_points='''
         [console_scripts]
