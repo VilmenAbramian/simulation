591687ccd49fee7943e3c4d6b979cabaa8a06aae
Vilmen <abramian.vl@phystech.edu>
2024-09-19 12:39:13 +0300

    Add parallel simulation

--- pysim/models/monte_carlo/cli.py
+++ pysim/models/monte_carlo/cli.py
@@ -1,5 +1,8 @@
 import click
+from multiprocessing import Pool
+import multiprocessing
 
+from processing import result_processing
 from pysim.sim.simulator import (
     build_simulation,
     run_simulation,
@@ -10,12 +13,35 @@ from pysim.models.monte_carlo.handlers import initialize, finalize
 
 
 MODEL_NAME = 'Monte-Carlo-simulation'
-DEFAULT_PROBABILITY = (1, 1, 1, 1)
-DEFAULT_PROCESSING_TIME = (1, 1, 1, 1)
-DEFAULT_MAX_TRANSMISSIONS = 200000
+DEFAULT_PROBABILITY = ((0.9, 0.95, 0.93, 0.91),)
+DEFAULT_PROCESSING_TIME = ((1, 1, 1, 1),)
+DEFAULT_MAX_TRANSMISSIONS = 2000
 DEFAULT_CHUNKS_NUMBER = 10
-DEFAULT_SCENARIO = 3
+DEFAULT_SCENARIO = 1
 SCENARIOS_TUPLE = (1, 2, 3)
+
+
+def check_vars(**kwargs):
+    '''
+    Проверка корректности введённых аргументов и
+    выбор режима работы симуляции (одиночный или
+    несколько моделей одновременно)
+    '''
+    if kwargs['scenario'] not in SCENARIOS_TUPLE:
+        raise AttributeError('Недопустимый номер сценария!')
+    if kwargs['scenario'] == 3 and kwargs['chunks_number'] < 1:
+        raise AttributeError('Недопустимое количество "чанков"!')
+    if len(kwargs['probability']) != len(kwargs['processing_time']):
+        raise AttributeError(
+            'Количество массивов времени и вероятностей не совпадает!'
+        )
+
+    if (len(kwargs['probability']) > 1 and
+           (type(kwargs['probability'][0]) is tuple)):
+        mode = 'plural'
+    else:
+        mode = 'single'
+    return mode
 
 
 @click.command()
@@ -23,6 +49,7 @@ SCENARIOS_TUPLE = (1, 2, 3)
     '-p', '--probability', nargs=4, type=click.Tuple(
         [float, float, float, float]
     ),
+    multiple=True,
     default=DEFAULT_PROBABILITY,
     help='Массив вероятностей перехода из одного состояния в другое',
     show_default=True
@@ -31,6 +58,7 @@ SCENARIOS_TUPLE = (1, 2, 3)
     '-t', '--processing_time', nargs=4, type=click.Tuple(
         [float, float, float, float]
     ),
+    multiple=True,
     default=DEFAULT_PROCESSING_TIME,
     help='Массив длительностей обработки пакета в каждом из состояний',
     show_default=True
@@ -55,23 +83,41 @@ def cli_run(**kwargs):
     Точка входа модели.
     Задать параметры работы.
     '''
-    if kwargs['scenario'] not in SCENARIOS_TUPLE:
-        raise AttributeError('Недопустимый номер сценария!')
-    if kwargs['scenario'] == 3 and kwargs['chunks_number'] < 1:
-        raise AttributeError('Недопустимое количество "чанков"!')
-
+    mode = check_vars(**kwargs)
     print(f'Running {MODEL_NAME} model. Scenario number {kwargs['scenario']}')
     print('Входные параметры: ', kwargs)
-    result = run_model(Config(
+    if mode == 'single':
+        kwargs['probability'] = kwargs['probability'][0]
+        kwargs['processing_time'] = kwargs['processing_time'][0]
+        result = create_config(kwargs)
+    else:
+        result = run_multiple_simulation(kwargs)
+    result_processing(kwargs, result)
+
+
+def create_config(kwargs):
+    # print(f'Что пришло на вход: {kwargs}')
+    return run_model(Config(
         probability=kwargs['probability'],
         processing_time=kwargs['processing_time'],
         max_transmisions=kwargs['max_transmisions'],
         chunks_number=kwargs['chunks_number'],
         scenario=kwargs['scenario']
     ), ModelLoggerConfig())
-    print('Суммарное время: ', result.sim_time)
-    print('Среднее время до поглощения: ',
-          result.sim_time/kwargs['max_transmisions'])
+
+
+def run_multiple_simulation(kwargs):
+    # Построим массив из копий параметров
+    args_list = [{
+        'probability': kwargs['probability'][i],
+        'processing_time': kwargs['processing_time'][i],
+        'max_transmisions': kwargs['max_transmisions'],
+        'chunks_number': kwargs['chunks_number'],
+        'scenario': kwargs['scenario'],
+    } for i in range(len(kwargs['probability']))]
+
+    pool = Pool(kwargs.get('jobs', multiprocessing.cpu_count()))
+    return pool.map(create_config, args_list)
 
 
 def run_model(
